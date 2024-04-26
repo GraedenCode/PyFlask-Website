bc12fa6cdcaa3b1392d39a7565b7d58eea7e8ac9
Graeden <BoyerCoding@gmail.com>
2024-04-26 12:00:58 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from website import create_app
+
+# Tutorial Video https://youtu.be/dam0GPOAvVI?si=pJEBc7v1624lNeYE&t=1336
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
